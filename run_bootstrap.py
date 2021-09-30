65f82fc7e3c81876c91ad0ce9a73911ca3884b45
superhuman <superhuman@eduroam-193-157-169-227.wlan.uio.no>
2021-09-30 17:21:41 +0200

    s

--- run_bootstrap.py
+++ run_bootstrap.py
@@ -195,7 +195,7 @@ def ptlearn(states, actions, rewards, next_states, terminal_flags, masks):
                 # loss of H(a|s,z)
                 logits = torch.softmax(q_policy_vals[k], dim=-1)
                 logits = torch.mean(torch.sum(logits*torch.log(logits), dim=-1))
-                l1loss += 0.01*logits
+                l1loss += 0.001*logits
                 entropy_loss.append(preds)
 
             full_loss = masks[:,k]*l1loss
@@ -204,11 +204,11 @@ def ptlearn(states, actions, rewards, next_states, terminal_flags, masks):
             losses[k] = loss.cpu().detach().item()
 
     loss = sum(cnt_losses)/info['N_ENSEMBLE']
-    # if 'entropy' in info['IMPROVEMENT']:
-    #     # loss of H(z|s)
-    #     logits = torch.softmax(torch.stack(entropy_loss), dim=0)
-    #     logits = torch.mean(torch.sum(logits*torch.log(logits), dim=0))
-    #     loss -= 0.01*logits
+    if 'entropy' in info['IMPROVEMENT']:
+        # loss of H(z|s)
+        logits = torch.softmax(torch.stack(entropy_loss), dim=0)
+        logits = torch.mean(torch.sum(logits*torch.log(logits), dim=0))
+        loss -= 0.001*logits
         
     loss.backward()
     for param in policy_net.core_net.parameters():
