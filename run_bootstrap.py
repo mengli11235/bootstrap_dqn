e8581bd7d6c0a6342ea152fb278508f33ffa1115
mengli11235 <mengli11235@gmail.com>
2021-10-08 13:04:01 +0200

    s

--- run_bootstrap.py
+++ run_bootstrap.py
@@ -204,7 +204,8 @@ def ptlearn(states, actions, rewards, next_states, terminal_flags, masks):
 #                 logits = torch.sum(logits*torch.log(logits), dim=-1) #batch
 #                 l1loss += 0.001*logits.mean() #1
                 preds = prior_q_policy_vals[k].gather(1, actions[:,None]).squeeze(1)
-                next_qs = 4 * torch.log(torch.sum(torch.exp(prior_next_q_target_vals[k]/4), dim=-1))
+                #next_qs = 4 * torch.log(torch.sum(torch.exp(prior_next_q_target_vals[k]/4), dim=-1))
+                next_qs = prior_next_q_target_vals[k].data.gather(1, next_actions).squeeze(1)
                 targets = rewards + info['GAMMA'] * next_qs * (1-terminal_flags)
                 l1loss += F.smooth_l1_loss(preds, targets, reduction='mean')
 
