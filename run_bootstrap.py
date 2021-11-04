1b586d1ec0a638be9ca283482f94c61e262adab2
superhuman <superhuman@eduroam-193-157-197-20.wlan.uio.no>
2021-11-04 14:12:21 +0100

    s

--- run_bootstrap.py
+++ run_bootstrap.py
@@ -60,7 +60,7 @@ def matplotlib_plot_all(p):
 
     plot_dict_losses({'eval rewards':{'index':np.array(p['eval_steps'])[eval_steps_mask], 'val':np.array(p['eval_rewards'])[eval_rewards_mask]}}, name=os.path.join(model_base_filedir, 'eval_rewards_steps.png'), rolling_length=0)
 
-    plot_dict_losses({'eval states':{'index':np.array(p['eval_steps'])[eval_steps_mask], 'val':np.array(p['eval_num_states'])[eval_rewards_mask]}}, name=os.path.join(model_base_filedir, 'eval_num_states_steps.png'), rolling_length=0)
+    #plot_dict_losses({'eval states':{'index':np.array(p['eval_steps'])[eval_steps_mask], 'val':np.array(p['eval_num_states'])[eval_rewards_mask]}}, name=os.path.join(model_base_filedir, 'eval_num_states_steps.png'), rolling_length=0)
 
 def handle_checkpoint(last_save, cnt):
     if (cnt-last_save) >= info['CHECKPOINT_EVERY_STEPS']:
@@ -136,11 +136,11 @@ class ActionGetter:
         Returns:
             An integer between 0 and n_actions
         """
-        state = torch.Tensor(state.astype(np.float)/info['NORM_BY'])[None,:].to(info['DEVICE'])
+        #state = torch.Tensor(state.astype(np.float)/info['NORM_BY'])[None,:].to(info['DEVICE'])
         # if 'discriminator' in info['IMPROVEMENT']:
         #     logits = discriminator(state, 0)
         #     #logits = torch.softmax(discriminator(states, 0), dim=-1)
-        #     #action_head = torch.argmax(logits, dim=-1).item()
+        #     action_head = torch.argmax(logits, dim=-1).item()
 
         if evaluation:
             eps = self.eps_evaluation
@@ -154,10 +154,10 @@ class ActionGetter:
                 eps = self.slope_2*step_number + self.intercept_2
         else:
             eps = 0
-        if self.random_state.rand() < eps: #or action_head == active_head:
+        if self.random_state.rand() < eps:
             return eps, self.random_state.randint(0, self.n_actions)
         else:
-            #state = torch.Tensor(state.astype(np.float)/info['NORM_BY'])[None,:].to(info['DEVICE'])
+            state = torch.Tensor(state.astype(np.float)/info['NORM_BY'])[None,:].to(info['DEVICE'])
             vals = policy_net(state, active_head)
             if active_head is not None:
                 action = torch.argmax(vals, dim=1).item()
@@ -165,13 +165,13 @@ class ActionGetter:
             else:
                 # vote
                 acts = [torch.argmax(vals[h],dim=1).item() for h in range(info['N_ENSEMBLE'])]
-                if 'discriminator' in info['IMPROVEMENT']:
-                    logits = discriminator(state, 0)
-                    action_head = torch.argmax(logits, dim=-1).item()
-                    action = acts[action_head]
-                else:
-                    data = Counter(acts)
-                    action = data.most_common(1)[0][0]
+                # if 'discriminator' in info['IMPROVEMENT']:
+                #     #logits = discriminator(state, 0)
+                #     action_head = torch.argmax(logits, dim=-1).item()
+                #     action = acts[action_head]
+                # else:
+                data = Counter(acts)
+                action = data.most_common(1)[0][0]
                 return eps, action
 
 def ptlearn(states, actions, rewards, next_states, terminal_flags, active_heads, masks):
@@ -331,7 +331,7 @@ def train(step_number, last_save):
 #                     print(len(perf['episode_reward']), step_number, perf['avg_rewards'][-1], file=reward_file)
         avg_eval_reward = evaluate(step_number)
         perf['eval_rewards'].append(avg_eval_reward)
-        perf['eval_num_states'].append(len(eval_states))
+        #perf['eval_num_states'].append(len(eval_states))
         perf['eval_steps'].append(step_number)
         matplotlib_plot_all(perf)
 
@@ -358,8 +358,8 @@ def evaluate(step_number):
             else:
                 eps,action = action_getter.pt_get_action(step_number, state, active_head=None, evaluation=True)
             next_state, reward, life_lost, terminal = env.step(action)
-            if next_state[-1].tobytes() not in eval_states:
-                eval_states.append(next_state[-1].tobytes())
+            # if next_state[-1].tobytes() not in eval_states:
+            #     eval_states.append(next_state[-1].tobytes())
             evaluate_step_number += 1
             episode_steps +=1
             episode_reward_sum += reward
@@ -378,7 +378,7 @@ def evaluate(step_number):
     # Show the evaluation score in tensorboard
     efile = os.path.join(model_base_filedir, 'eval_rewards.txt')
     with open(efile, 'a') as eval_reward_file:
-        print(step_number, np.mean(eval_rewards), len(eval_states), file=eval_reward_file)
+        print(step_number, np.mean(eval_rewards), file=eval_reward_file)
     return np.mean(eval_rewards)
 
 if __name__ == '__main__':
@@ -440,7 +440,7 @@ if __name__ == '__main__':
         "FRAME_SKIP":4, # deterministic frame skips to match deepmind
         "MAX_NO_OP_FRAMES":30, # random number of noops applied to beginning of each episode
         "DEAD_AS_END":True, # do you send finished=true to agent while training when it loses a life
-        "IMPROVEMENT": ['discriminator'],
+        "IMPROVEMENT": [],
     }
 
     info['FAKE_ACTS'] = [info['RANDOM_HEAD'] for x in range(info['N_ENSEMBLE'])]
@@ -498,7 +498,7 @@ if __name__ == '__main__':
                 'episode_times':[],
                 'episode_relative_times':[],
                 'eval_rewards':[],
-                'eval_num_states':[],
+                #'eval_num_states':[],
                 'eval_steps':[]}
 
         start_step_number = 0
@@ -532,21 +532,20 @@ if __name__ == '__main__':
                                 n_actions=env.num_actions,
                                 network_output_size=info['NETWORK_INPUT_SIZE'][0],
                                 num_channels=info['HISTORY_SIZE'], dueling=info['DUELING']).to(info['DEVICE'])
-        prior_target_net = EnsembleNet(n_ensemble=info['N_ENSEMBLE'],
-                                n_actions=env.num_actions,
-                                network_output_size=info['NETWORK_INPUT_SIZE'][0],
-                                num_channels=info['HISTORY_SIZE'], dueling=info['DUELING']).to(info['DEVICE'])
+        # prior_target_net = EnsembleNet(n_ensemble=info['N_ENSEMBLE'],
+        #                         n_actions=env.num_actions,
+        #                         network_output_size=info['NETWORK_INPUT_SIZE'][0],
+        #                         num_channels=info['HISTORY_SIZE'], dueling=info['DUELING']).to(info['DEVICE'])
 
-        discriminator = EnsembleNet(n_ensemble=1,
-                                n_actions=info['N_ENSEMBLE'],
-                                network_output_size=info['NETWORK_INPUT_SIZE'][0],
-                                num_channels=info['HISTORY_SIZE'], dueling=False).to(info['DEVICE'])
+        # discriminator = EnsembleNet(n_ensemble=1,
+        #                         n_actions=info['N_ENSEMBLE'],
+        #                         network_output_size=info['NETWORK_INPUT_SIZE'][0],
+        #                         num_channels=info['HISTORY_SIZE'], dueling=False).to(info['DEVICE'])
         print("using randomized prior")
         policy_net = NetWithPrior(policy_net, prior_net, info['PRIOR_SCALE'])
-        target_net = NetWithPrior(target_net, prior_target_net, info['PRIOR_SCALE'])
+        target_net = NetWithPrior(target_net, prior_net, info['PRIOR_SCALE'])
 
     target_net.load_state_dict(policy_net.state_dict())
-    #prior_target_net.load_state_dict(prior_net.state_dict())
 
     # create optimizer
     #opt = optim.RMSprop(policy_net.parameters(),
@@ -560,7 +559,7 @@ if __name__ == '__main__':
 
     kl_loss = nn.KLDivLoss()
     ce_loss = nn.CrossEntropyLoss()
-    eval_states = []
+    #eval_states = []
     if args.model_loadpath is not '':
         # what about random states - they will be wrong now???
         # TODO - what about target net update cnt
