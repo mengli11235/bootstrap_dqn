e21b111cc83f44c7d2040648356e61ce697c442e
superhuman <superhuman@eduroam-193-157-179-45.wlan.uio.no>
2021-12-09 15:05:23 +0100

    s

--- run_bootstrap.py
+++ run_bootstrap.py
@@ -594,7 +594,7 @@ if __name__ == '__main__':
         "MAX_NO_OP_FRAMES":30, # random number of noops applied to beginning of each episode
         "DEAD_AS_END":True, # do you send finished=true to agent while training when it loses a life
         "SURGE_INTERVAL":2e5,
-        "IMPROVEMENT": ['PRIOR', ''],
+        "IMPROVEMENT": ['', ''],
     }
 
     info['FAKE_ACTS'] = [info['RANDOM_HEAD'] for x in range(info['N_ENSEMBLE'])]
