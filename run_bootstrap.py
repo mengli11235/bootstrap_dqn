035636c13211093c82b58a87e7ca3eb9bf417e54
superhuman <superhuman@superhumandeMBP.lan>
2021-12-21 14:04:25 +0100

    s

--- run_bootstrap.py
+++ run_bootstrap.py
@@ -554,7 +554,7 @@ if __name__ == '__main__':
     print("running on %s"%device)
 
     info = {
-        "GAME":'roms/breakout.bin', # gym prefix
+        "GAME":'roms/qbert.bin', # gym prefix
         #"GAME":'roms/freeway.bin', # gym prefix
         "DEVICE":device, #cpu vs gpu set by argument
         "NAME":'FRANKbootstrap_fasteranneal_pong', # start files with name
