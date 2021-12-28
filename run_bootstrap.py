d55710062d2b01262351e3185e31d6002b49272c
superhuman <superhuman@superhumandeMBP.lan>
2021-12-28 17:08:26 +0100

    s

--- run_bootstrap.py
+++ run_bootstrap.py
@@ -217,7 +217,7 @@ def ptlearn(states, actions, rewards, next_states, terminal_flags, active_heads,
         q_record = torch.stack(next_q_target_vals).detach().max()
         #info['PRIOR_SCALE'] = 1+torch.stack(next_q_target_vals).detach().max()//20
         prior_next_pi = torch.empty(info['N_ENSEMBLE'], info['BATCH_SIZE'], q_policy_vals[0].size(-1)).to(info['DEVICE'])
-        nn.init.normal_(prior_next_pi, 0, 0.05)
+        nn.init.normal_(prior_next_pi, 0, 0.01)
         prior_next_pi = 1- prior_next_pi
 
     elif 'PRETRAIN' in info['IMPROVEMENT']:
