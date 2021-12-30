b62fdf72af565b18b6788c868cd02a4685bdd0e4
superhuman <superhuman@superhumandeMBP.lan>
2021-12-30 14:50:18 +0100

    s

--- run_bootstrap.py
+++ run_bootstrap.py
@@ -215,10 +215,10 @@ def ptlearn(states, actions, rewards, next_states, terminal_flags, active_heads,
         # prior_pi = torch.empty(info['N_ENSEMBLE'], info['BATCH_SIZE'],  q_policy_vals[0].size(-1)).to(info['DEVICE'])
         # nn.init.normal_(prior_pi, 0, 0.02)
         q_record = torch.stack(next_q_target_vals).detach().max()
-        #info['PRIOR_SCALE'] = 1+torch.stack(next_q_target_vals).detach().max()//20
+        info['PRIOR_SCALE'] = torch.stack(next_q_target_vals).detach().max()
         prior_next_pi = torch.empty(info['N_ENSEMBLE'], info['BATCH_SIZE'], q_policy_vals[0].size(-1)).to(info['DEVICE'])
-        nn.init.normal_(prior_next_pi, 0, 0.001)
-        prior_next_pi = 1- prior_next_pi
+        nn.init.normal_(prior_next_pi, 0, 0.02)
+        #prior_next_pi = 1- prior_next_pi
 
     elif 'PRETRAIN' in info['IMPROVEMENT']:
         prior_pi = prior_net.forward(states, return_all_heads=True)
@@ -252,8 +252,11 @@ def ptlearn(states, actions, rewards, next_states, terminal_flags, active_heads,
                 #print(prior_next_pi.size())
                 
                 #info['PRIOR_SCALE'] = 1+next_q_vals.max()//20
-                next_q_vals *= info['PRIOR_SCALE']*prior_next_pi[next_k].detach()
-                next_policy_vals *= info['PRIOR_SCALE']*prior_next_pi[next_k].detach()
+                next_q_vals += info['PRIOR_SCALE']*prior_next_pi[next_k].detach()
+                next_policy_vals += info['PRIOR_SCALE']*prior_next_pi[next_k].detach()
+
+                #next_q_vals *= info['PRIOR_SCALE']*prior_next_pi[next_k].detach()
+                #next_policy_vals *= info['PRIOR_SCALE']*prior_next_pi[next_k].detach()
 
             if info['DOUBLE_DQN']:
                 next_actions = next_policy_vals.max(1, True)[1]
