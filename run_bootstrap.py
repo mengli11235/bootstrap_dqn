367064f826d955dfaf11dd54126b0b43ee660dc4
Li Meng <limeng@ml7.hpc.uio.no>
2022-02-09 19:50:23 +0100

    s

--- run_bootstrap.py
+++ run_bootstrap.py
@@ -400,7 +400,7 @@ if __name__ == '__main__':
     print("running on %s"%device)
 
     info = {
-        "GAME":'roms/gravitar.bin', # gym prefix
+        "GAME":'roms/enduro.bin', # gym prefix
         #"GAME":'roms/freeway.bin', # gym prefix
         "DEVICE":device, #cpu vs gpu set by argument
         "NAME":'FRANKbootstrap_fasteranneal_pong', # start files with name
